f545fbe632624e8f6d57b4998fd5e0466a9dc1d4
danielcreis <daniel.cvt7@gmail.com>
2024-03-03 01:15:09 -0300

    Finally Project

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 "use client";
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import {
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { FaWhatsapp } from "react-icons/fa";
+
 import Image from "next/image";
 import deved from "../../public/deved.png";
 import design from "../../public/design.png";
+import study from "../../public/study.png";
 import code from "../../public/code.png";
 import web1 from "../../public/web1.png";
 import web2 from "../../public/web2.png";
 import web3 from "../../public/web3.png";
 import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
-import web6 from "../../public/web6.png";
+import web7 from "../../public/web7.png";
 import { useState } from "react";
 
 export default function Home() {
@@ -35,14 +34,6 @@ export default function Home() {
                   className="cursor-pointer text-2xl  text-black dark:text-white"
                 />
               </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href=""
-                >
-                  Resumo
-                </a>
-              </li>
             </ul>
           </nav>
           <div className="text-center p-10 ">
@@ -57,10 +48,25 @@ export default function Home() {
               portfólio e veja como estou construindo o futuro da tecnologia
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillYoutube />
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600  dark:text-white">
+            <a
+              href="https://www.linkedin.com/in/danielcvtreis/"
+              target="_blank"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a href="https://github.com/danielcreis" target="_blank">
+              <AiFillGithub />
+            </a>
+            <a href="https://wa.me/5521977150404" target="_blank">
+              <FaWhatsapp />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCoBiOuKaUl2iOT_cywH3prA"
+              target="_blank"
+            >
+              <AiFillYoutube />
+            </a>
           </div>
           <div className="relative mx-auto  bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96 ">
             <Image
@@ -74,8 +80,17 @@ export default function Home() {
         <section>
           <div>
             <h3 className="text-3xl py-1  text-black  dark:text-white">
-              Serviços I Ofertas
+              Sobre mim
             </h3>
+            <p className="text-md py-2 leading-8 text-gray-800  dark:text-white">
+              Minha paixão pela programação me levou a estudar e me aprofundar
+              em desenvolvimento web, desde o design de interfaces até a
+              implementação de funcionalidades complexas em diversas linguagens
+              de programação. Acredito que a tecnologia é uma ferramenta
+              poderosa para conectar pessoas e transformar negócios, e meu
+              objetivo é contribuir para o desenvolvimento de soluções
+              tecnológicas inovadoras e impactantes.
+            </p>
             <p className="text-md py-2 leading-8  text-gray-800  dark:text-white">
               Conquistei clientes valiosos como freelancer, deixando minha marca
               em cada projeto. Como desenvolvedor, contribuí de forma
@@ -87,13 +102,30 @@ export default function Home() {
                 desenvolvimento de aplicativos e softwares.
               </span>
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800  dark:text-white">
-              Ofereço uma gama diversificada de serviços, abrangendo
-              programação, solução de problemas, educação e habilidades
-              interpessoais.
-            </p>
           </div>
           <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <Image src={code} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2  text-gray-800  dark:text-white">
+                Programador
+              </h3>
+              <p className="py-2  text-gray-800  dark:text-white">
+                Desenvolvendo soluções elegantes com base nos princípios da
+                programação e dos códigos.
+              </p>
+              <h4 className="py-4 text-teal-600">Linguagens | Ferramentas</h4>
+              <p className="text-gray-800 py-1  dark:text-white">
+                JavaScript | NodeJS
+              </p>
+              <p className="text-gray-800 py-1  dark:text-white">
+                React | React Native
+              </p>
+              <p className="text-gray-800 py-1  dark:text-white">AWS</p>
+              <p className="text-gray-800 py-1  dark:text-white">Python</p>
+              <p className="text-gray-800 py-1  dark:text-white">
+                Git e Github
+              </p>
+            </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
               <Image
                 src={design}
@@ -101,69 +133,48 @@ export default function Home() {
                 height={100}
                 className="mx-auto"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2  text-gray-800  dark:text-white">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600 ">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1  dark:text-white">Photoshop</p>
-              <p className="text-gray-800 py-1  dark:text-white">Illustrator</p>
-              <p className="text-gray-800 py-1  dark:text-white">Figma</p>
-              <p className="text-gray-800 py-1  dark:text-white">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
-              <Image src={code} width={100} height={100} className="mx-auto" />
               <h3 className="text-lg font-medium pt-8 pb-2  text-gray-800  dark:text-white">
-                Beautiful Designs
+                Design
               </h3>
               <p className="py-2  text-gray-800  dark:text-white">
-                Creating elegant designs suited for your needs following core
-                design theory.
+                Criando designs elegantes adequados às suas necessidades,
+                seguindo a teoria fundamental do design.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <h4 className="py-4 text-teal-600 ">Design | Ferramentas</h4>
               <p className="text-gray-800 py-1  dark:text-white">Photoshop</p>
               <p className="text-gray-800 py-1  dark:text-white">Illustrator</p>
               <p className="text-gray-800 py-1  dark:text-white">Figma</p>
-              <p className="text-gray-800 py-1  dark:text-white">Indesign</p>
+              <p className="text-gray-800 py-1  dark:text-white">Canvas</p>
             </div>
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
-              <Image src={code} width={100} height={100} className="mx-auto" />
+              <Image src={study} width={100} height={100} className="mx-auto" />
               <h3 className="text-lg font-medium pt-8 pb-2  text-gray-800  dark:text-white">
-                Beautiful Designs
+                Formação Superior
               </h3>
               <p className="py-2  text-gray-800  dark:text-white">
-                Creating elegant designs suited for your needs following core
-                design theory.
+                Formado em Análise e desenvolvimentos de sistemas pela Estácio
+                de Sá.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1  dark:text-white">Photoshop</p>
-              <p className="text-gray-800 py-1  dark:text-white">Illustrator</p>
-              <p className="text-gray-800 py-1  dark:text-white">Figma</p>
-              <p className="text-gray-800 py-1  dark:text-white">Indesign</p>
+              <h4 className="py-4 text-teal-600">Atualmente estudando</h4>
+              <p className="text-gray-800 py-1  dark:text-white">IA</p>
+              <p className="text-gray-800 py-1  dark:text-white">Java</p>
+              <p className="text-gray-800 py-1  dark:text-white">Rust</p>
+              <p className="text-gray-800 py-1  dark:text-white">Dados</p>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1">Portfolio</h3>
+            <h3 className="text-3xl py-1">Projetos</h3>
             <p className="text-md py-2 leading-8  text-gray-800  dark:text-white">
-              Conquistei clientes valiosos como freelancer, deixando minha marca
-              em cada projeto. Como desenvolvedor, contribuí de forma
-              significativa para o sucesso das{" "}
-              <span className="text-teal-500"> empresas </span> por onde passei,
-              colaborando com talentosos colegas na resolução de problemas e no
-              <span className="text-teal-500">
-                {" "}
-                desenvolvimento de aplicativos e softwares.
-              </span>
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-80">
-              Ofereço uma gama diversificada de serviços, abrangendo
-              programação, solução de problemas, educação e habilidades
-              interpessoais.
+              Explorando as fronteiras da inovação, meus projetos abaixo são um
+              testemunho do meu compromisso com o aprendizado contínuo e a busca
+              incansável pela{" "}
+              <span className="text-teal-500">excelência técnica</span>. Cada
+              empreendimento é uma oportunidade para adquirir novos
+              conhecimentos e aprimorar minhas habilidades, consolidando minha
+              jornada como um desenvolvedor apaixonado pela arte da programação.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
@@ -209,7 +220,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web5}
+                src={web7}
               />
             </div>
             <div className="basis-1/3 flex-1">
@@ -218,7 +229,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web6}
+                src={web5}
               />
             </div>
           </div>
